11b5ec714ff7899407263e0f29e7be0b4376ef11
Fabian Bonilla <facabore@gmail.com>
2024-07-03 03:40:36 -0500

    Delete AccessKeyPolly

--- NumbersToVoice/obj/Debug/net8.0/NumbersToVoice.AssemblyInfo.cs
+++ NumbersToVoice/obj/Debug/net8.0/NumbersToVoice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumbersToVoice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9513e27b2e94e8683ce590fea1cbff52bd6d1660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51241a796d2c5707d97678d701756b019fa727f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumbersToVoice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumbersToVoice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
