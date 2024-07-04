102a90fd10df69136e6f68e8cb1662910591f3ca
Fabian Bonilla <facabore@gmail.com>
2024-07-04 04:38:28 -0500

    Update RegularExpression
    Now accept minus numbers

--- NumbersToVoice/Entities/Number/NumberRequest.cs
+++ NumbersToVoice/Entities/Number/NumberRequest.cs
@@ -4,7 +4,7 @@ namespace NumbersToVoice.Entities;
 
 public class NumberRequest
 {
-    [RegularExpression(@"^\d{1,12}", ErrorMessage = "The number need between range from 0 to 999.999.999.999.")]
+    [RegularExpression(@"^-?\d{1,12}", ErrorMessage = "The number need between range from -999.999.999.999 to 999.999.999.999.")]
     public long Number { get; set; }
 
 }
